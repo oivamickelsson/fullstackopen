885b11633af6dbb86d65779a4d52a758ff8455b6
Oiva Mickelsson <oomick@utu.fi>
2023-08-23 15:34:05 +0300

    small changes

--- part3/phonebackend/index.js
+++ part3/phonebackend/index.js
@@ -1,53 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-app.use(express.json())
-var morgan = require('morgan')
-const cors = require('cors')
-app.use(cors())
 app.use(express.static('dist'))
 
-
-app.use(morgan('tiny', {
-  skip: function (req, res) { return req.method === 'POST' }
-}))
-
-morgan.token('person-content', function getPersonContent (req) {
-  return JSON.stringify(req.body)
-})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person-content', {
-  skip: function (req, res) {return req.method !== 'POST'}
-}))
-
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-  return maxId + 1
+app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
 }
+app.use(requestLogger)
 
 
 const dateOptions = {
@@ -79,62 +43,141 @@ app.get('/info', (request, response) => {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   const formattedDate = `${month} ${day} ${year} ${hours}:${minutes}:${seconds} GMT+${now.getTimezoneOffset() / -60}00 (${timeZone})`;
-  response.send(`<p> Phonebook has info for ${persons.length} people </p> <p> ${formattedDate} </p>`);
+  response.send(`<p> Phonebook has info for ${Person.length} people </p> <p> ${formattedDate} </p>`);
 });
 
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person) 
+    } else {
+      
+      response.status(404).end()
+    }
+  })
+  .catch(error => {next(error)})
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+
+
+app.put('/api/persons/:id', (request, response, next) => {
   
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  const { name, number } = request.body
+
+
+
+  Person.findByIdAndUpdate(request.params.id, {name, number}, { new: true , runValidators:true, context:'query'})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
 
-  response.status(204).end()
-})
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  console.log('Request Body:', request.body)
 
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
- 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+    })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
 })
 
+var morgan = require('morgan')
+const cors = require('cors')
+app.use(cors())
+const Person = require('./models/person')
 
 
-const PORT = process.env.PORT || 3001
+
+app.use(morgan('tiny', {
+  skip: function (req, res) { return req.method === 'POST' }
+}))
+
+morgan.token('person-content', function getPersonContent (req) {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person-content', {
+  skip: function (req, res) {return req.method !== 'POST'}
+}))
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+/*
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+*/
+/*
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+*/
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+
+
+
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
